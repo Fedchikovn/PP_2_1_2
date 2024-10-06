ff2222acea85992d00c87bb3904e7469c9fc1cbc
nikitafedcikov <fiedchikov@mail.ru>
2024-10-07 02:53:09 +0700

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = applicationContext.getBean("cat", Cat.class);
+        Cat beanCat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean1 == bean2);
+        System.out.println(beanCat1 == beanCat2);
+
+//        System.out.println(bean.getMessage());
+    }
+}
